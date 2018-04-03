9f9ef322265df5bf431f01d674109953f965aafb
SpongeIsNotBoB <karachunvladislav@gmail.com>
2018-04-03 13:46:33 +0300

    first commit

---
+++ CollectionImage/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionImage
+//
+//  Created by Vladislav on 3/30/18.
+//  Copyright © 2018 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
